479a2441d2449f27cbda6aa1bd625c5d5e802e29
Salvador Carrillo <Salvador.Carrillo1@partner.magna.com>
2021-03-02 17:58:55 -0600

    added addItem and Remove Item functionality to checkout item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -10,5 +10,21 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 
+}
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+
+    const existingCarItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id)
+
+    if (existingCarItem.quantity === 1) { // si la cantidad es uno quitamos el item de la lista
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+
+    return cartItems.map( // si la cantidad no es uno , restamos la cantidad del item
+        cartItem =>
+            cartItem.id === cartItemToRemove.id ?
+                { ...cartItem, quantity: cartItem.quantity - 1 }
+                : cartItem
+    )
 
 }
